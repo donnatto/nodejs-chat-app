a532297d0726082ac947ccd6070a992a48772eab
Edwin Dominguez <edwindominguez@hotmail.com>
2022-01-13 17:32:06 -0500

    add socket io and add chat events

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
-const app = require('./app')
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
+const publicDirPath = path.join(__dirname, '../public')
 
-app.listen(port, () => {
-  console.log('Server is up on port', port)
+app.use(express.static(publicDirPath))
+
+io.on('connection', (socket) => {
+  console.log('New websocket connection')
+
+  socket.emit('message', 'Welcome!')
+
+  socket.on('sendMessage', (message) => {
+    io.emit('message', message)
+  })
+})
+
+server.listen(port, () => {
+  console.log(`Server is up on port ${port}`)
 })
