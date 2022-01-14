a5974ac40f2e95cc1c7c0035dc8240604660eec3
Edwin Dominguez <edwindominguez@hotmail.com>
2022-01-13 19:45:26 -0500

    handle button and form states

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,15 +1,27 @@
 const socket = io()
 
+// Elements
+const $messageForm = document.getElementById('message-form')
+const $messageFormInput = $messageForm.querySelector('input')
+const $messageFormButton = $messageForm.querySelector('button')
+const $sendLocationButton = document.getElementById('send-location')
+
 socket.on('message', (message) => {
   console.log(message)
 })
 
-document
-  .querySelector('#message-form')
+$messageForm
   .addEventListener('submit', (e) =>{
     e.preventDefault()
+
+    // disable
+    $messageFormButton.setAttribute('disabled', 'disabled')
     const message = e.target.elements.message.value
     socket.emit('sendMessage', message, (error) => {
+      // enable
+      $messageFormButton.removeAttribute('disabled')
+      $messageFormInput.value = ''
+      $messageFormInput.focus()
       if (error) {
         return console.log(error)
       }
@@ -17,16 +29,19 @@ document
     })
   })
 
-document
-  .getElementById('send-location')
+$sendLocationButton
   .addEventListener('click', e => {
     if (!navigator.geolocation) {
       return alert('Geolocation is not supported by your browser.')
     }
+    $sendLocationButton.setAttribute('disabled', 'disabled')
     navigator.geolocation.getCurrentPosition((position) => {
       socket.emit('sendLocation', {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude
-      }, () => console.log('Location shared!'))
+      }, () => {
+        $sendLocationButton.removeAttribute('disabled')
+        console.log('Location shared!')
+      })
     })
   })
