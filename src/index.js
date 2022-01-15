35e66bdb022b94bec4bc1846308edfc9b3d09859
Edwin Dominguez <edwindominguez@hotmail.com>
2022-01-15 11:24:51 -0500

    add usernames to messages

--- src/index.js
+++ src/index.js
@@ -25,34 +25,42 @@ io.on('connection', (socket) => {
     }
     socket.join(user.room)
 
-    socket.emit('message', generateMessage('Welcome'))
-    socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`))
+    socket.emit('message', generateMessage('Admin','Welcome'))
+    socket.broadcast.to(user.room).emit('message', generateMessage('Admin',`${user.username} has joined!`))
 
     callback()
   })
 
   socket.on('sendMessage', (message, callback) => {
-    const filter = new Filter()
+    const user = getUser(socket.id)
+    if (user) {
+      const filter = new Filter()
 
-    if (filter.isProfane(message)) {
-      return callback('Profanity is not allowed!')
+      if (filter.isProfane(message)) {
+        return callback('Profanity is not allowed!')
+      }
+      io.to(user.room).emit('message', generateMessage(user.username, message))
+      callback()
     }
-    io.emit('message', generateMessage(message))
-    callback()
   })
 
   socket.on('disconnect', () => {
     const user = removeUser(socket.id)
 
     if (user) {
-      io.to(user.room).emit('message', generateMessage(`${user.username} has left!`))
+      io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`))
     }
 
   })
 
   socket.on('sendLocation', (coords, callback) => {
-    io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
-    callback()
+    const user = getUser(socket.id)
+    if (user) {
+      io.to(user.room).emit('locationMessage', generateLocationMessage(
+        user.username,
+        `https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
+      callback()
+    }
   })
 })
 
